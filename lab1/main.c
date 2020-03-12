051c2ca24d2b688a5428b72525a297c99bef0d47
Egemenatilla <44278787+Egemenatilla@users.noreply.github.com>
2020-03-12 18:02:06 +0300

    Add files via upload

---
+++ lab1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //printf("\\n yeni satira gecer");
+
+   /* int a,b;
+    int toplam;
+    printf("Birinci sayiyi giriniz.");
+    scanf("%d",&a);
+    printf("Ikinci sayiyi giriniz.");
+    scanf("%d",&b);
+    toplam=a+b;
+
+    printf("%d ",toplam);
+*/
+  /*  int a,b,c;
+    printf("birinci sayi:");
+    scanf("%d",&a);
+    printf("ikinci sayi:");
+    scanf("%d",&b);
+    printf("ucuncu sayi:");
+    scanf("%d",&c);
+    float islem;
+    islem = (a-1+b+2+3*c)/3;
+    printf("%f",islem);
+   */
+    return 0;
+}
